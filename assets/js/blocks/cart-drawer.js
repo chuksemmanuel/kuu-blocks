8ade0883108aaccc6700985c7dd00901b4d40441
Chuks Emmanuel <dev.chuksemmanuel@gmail.com>
2025-11-16 17:16:24 +0100

    feat: Implement quick add functionality for products
    - Added a new quick add feature in cart-drawer.js to allow users to add products to the cart without leaving the current page.
    - Updated featured_products.liquid to include a new class for the product grid.
    - Refactored add-to-cart-button.liquid to utilize the new quick add data structure.
    - Enhanced product-card.liquid to wrap the quick add button in a div for better styling.
    - Created quick-add-content.liquid to manage the quick add modal content and logic.
    - Updated tailwind.css to improve focus styles.
    - Added JavaScript to manage quick add data and interactions.

--- assets/js/blocks/cart-drawer.js
+++ assets/js/blocks/cart-drawer.js
@@ -261,3 +261,51 @@ document.addEventListener('alpine:init', () => {
         }
     }))
 })
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('alpine:init', () => {
+    const Alpine = window.Alpine
+    Alpine.data('quick-add{{id}}', () => ({
+        adding: false,
+        async handleAddToCart() {
+            console.log(this.adding)
+            this.adding = true
+            console.log(this.adding)
+
+            try {
+                await fetch('/cart/add.js', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        items: [
+                            {
+                                id: '{{id}}',
+                                quantity: 1
+                            }
+                        ]
+                    })
+                })
+
+                let cartDrawer = window.theme?.cartDrawer
+                if (cartDrawer) {
+                    await cartDrawer.refresh()
+                }
+                cartDrawer.open()
+            } catch (e) {
+                console.log(e)
+            } finally {
+                this.adding = false
+            }
+        }
+    }))
+})
