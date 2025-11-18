8e15e8b0739678ad8d9de4a797cf76534053f4f3
Chuks Emmanuel <dev.chuksemmanuel@gmail.com>
2025-11-18 23:12:38 +0100

    feat: Implement quick add functionality with Alpine.js for add-to-cart button

---
+++ assets/js/components/add-to-cart-btn.js
@@ -0,0 +1,39 @@
+document.addEventListener('alpine:init', () => {
+    const Alpine = window.Alpine
+    Alpine.data('quickadd', (/** @type {HTMLElement} */ root) => ({
+        adding: false,
+        id: root.dataset.id,
+        quantity: root.dataset.quantity,
+        async handleAddToCart() {
+            this.adding = true
+
+            try {
+                await fetch('/cart/add.js', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        items: [
+                            {
+                                id: this.id,
+                                quantity: this.quantity
+                            }
+                        ]
+                    })
+                })
+
+                let cartDrawer = window.theme?.cartDrawer
+                if (cartDrawer) {
+                    await cartDrawer.refresh()
+                    cartDrawer.open()
+                }
+
+            } catch (e) {
+                console.log(e)
+            } finally {
+                this.adding = false
+            }
+        }
+    }))
+});
